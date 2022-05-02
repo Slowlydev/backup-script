7f21b849a15a74782c60b08ce09147a75a820fb4
Slowlydev <slowly.developers@gmail.com>
2022-05-02 11:27:12 +0200

    feat: basic reading and writing to config file

--- setup.sh
+++ setup.sh
@@ -2,14 +2,34 @@
 # backup setup script
 
 # TODO: ask user about backup target and storage location
-# your sophisticated code here
+# TODO: ask for backup cronjob 
+# TODO: convert to arrays of folders to backup
 
-# replaces $1 with $2 in config file
+# sets key $1 with value $2 in config file
 storeToConfig() {
-  sed -i '' "s/${1}/${2}/g" backup.config
+  sed -i '' "s/\(${1} *= *\).*/\1${2//\//\\/}/" backup.config
 }
 
-# write directories to config file
-# TODO write function which replaces slash with backslash slash
-storeToConfig "replaceTargetDirectory" "\/Your\/Files\/To\/Backup"
-storeToConfig "replaceBackupDirectory" "\/Your\/backup\/Directory"
+# check if backup file exsists
+if [[ -s "backup.config" ]]; then
+  source "backup.config"
+else
+  echo "fatal: no config file found"
+  exit 1
+fi
+
+# welcome message
+echo ""
+echo "Welcome to the backup Setup"
+echo ""
+
+# get and save directory for backups 
+echo "Please enter the directory where u want to store your compressed backups"
+read -p "Backup directory: " backupDirectory
+storeToConfig "backupDirectory" $backupDirectory
+
+# get and save direcotroy to backup
+echo "Please enter the directory u want to backup"
+read -p "Direcotry to backup: " targetDirectory
+storeToConfig "targetDirectory" $targetDirectory
+
