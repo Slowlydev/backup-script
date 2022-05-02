40622c25d32945f51fe6246cad95ccea1b6033e9
Matthias Mantsch <matthiasmantsch@Matthiass-MacBook-Pro.local>
2022-05-02 14:51:20 +0200

    Build algorythm further

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# backup restore script
 
+# backup restore script
 if [[ -s "backup.config" ]]; then
   source "backup.config"
 else
@@ -8,6 +8,7 @@ else
   exit 1
 fi
 
+#Info about your backup directory's
 echo "info: target directory ${targetDirectory}"
 echo "info: backup directory ${backupDirectory}"
 echo "info: timestamp format ${timestampFormat}"
@@ -35,23 +36,67 @@ fourthFile="$(ls -1 | head -4 | tail -1)"
 fifthFile="$(ls -1 | head -5 | tail -1)"
 
 
+function transferCheck {
+  echo ""
+  echo "Do you really wanna transfer the files"
+  read -p "yes/no: " $readAnswer
+  if [[ $readAnswer -eq "yes" ]]; then
+    echo "Transfering the directory"
+    cd $backupDirectory
+  else 
+    exit 1
+  fi
+}
+
+function transferFile {
+  cd backup
+  pwd
+  newFile="$(ls -1 | head -1)"
+  echo $newFile
+  sleep 1
+  cp $newFile $targetDirectory
+  rm $newFile
+  cd ..
+  rmdir backup
+}
+
 #Case Statemeant that gonna choose the file to restore
 case $userFileName in 
 
 "1")
-  echo $firstFile ;;
+  echo $firstFile 
+  transferCheck
+  nice -n 19 tar -xf $firstFile
+  transferFile
+  ;;
 
 "2")
-  echo $secondFile ;;
+  echo $secondFile 
+  transferCheck
+  nice -n 19 tar -xf $secondFile
+  transferFile
+  ;;
 
 "3")
-  echo $thirdFile ;;
+  echo $thirdFile 
+  transferCheck
+  nice -n 19 tar -xf $thirdFile
+  transferFile
+  ;;
 
 "4") 
-  echo $fourthFile ;;
+  echo $fourthFile 
+  transferCheck
+  nice -n 19 tar -xf $fourthFile
+  transferFile
+  ;;
 
 "5") 
-  echo $fifthFile ;;
+  echo $fifthFile 
+  transferCheck
+  nice -n 19 tar -xf $fifthFile
+  transferFile
+  ;;
 
 "6")
   read -p "Please enter the file name: " $selfChosenFile
