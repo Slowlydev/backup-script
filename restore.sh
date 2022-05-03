6da1d232fef2652e0550ba0e9762298ec4d04e73
Matthias Mantsch <matthiasmantsch@Matthiass-MBP.net>
2022-05-03 15:20:03 +0200

    feature: cp and deleting files

--- restore.sh
+++ restore.sh
@@ -25,7 +25,7 @@ echo ""
 ls -1 | head -5
 sleep 2
 echo ""
-echo "Please type a number between 1 and 5 or write 6 to manually choose a file"
+echo "Do you wanna have the newest Backup(1) or write 2 to manually choose a file"
 read -p "Your Number: " userFileName
 
 
@@ -34,7 +34,7 @@ secondFile="$(ls -1 | head -2 | tail -1)"
 thirdFile="$(ls -1 | head -3 | tail -1)"
 fourthFile="$(ls -1 | head -4 | tail -1)"
 fifthFile="$(ls -1 | head -5 | tail -1)"
-
+#transferDirectory="$(ls | grep -E '(^|\s)backup($|\s)')"
 
 function transferCheck {
   echo ""
@@ -49,15 +49,12 @@ function transferCheck {
 }
 
 function transferFile {
-  cd backup
+  echo ""
   pwd
-  newFile="$(ls -1 | head -1)"
-  echo $newFile
+  echo backup
   sleep 1
-  cp $newFile $targetDirectory
-  rm $newFile
-  cd ..
-  rmdir backup
+  cp -r backup $targetDirectory
+  rm -r backup
 }
 
 #Case Statemeant that gonna choose the file to restore
@@ -71,42 +68,18 @@ case $userFileName in
   ;;
 
 "2")
-  echo $secondFile 
-  transferCheck
-  nice -n 19 tar -xf $secondFile
-  transferFile
-  ;;
-
-"3")
-  echo $thirdFile 
-  transferCheck
-  nice -n 19 tar -xf $thirdFile
-  transferFile
-  ;;
-
-"4") 
-  echo $fourthFile 
-  transferCheck
-  nice -n 19 tar -xf $fourthFile
-  transferFile
-  ;;
-
-"5") 
-  echo $fifthFile 
-  transferCheck
-  nice -n 19 tar -xf $fifthFile
-  transferFile
-  ;;
-
-"6")
   read -p "Please enter the file name: " $selfChosenFile
   if [[ -s $selfChosenFileName ]]; then
     echo "The file $selfChosenFile exists"
+    sleep 1
+    transferCheck
+    nice -n 19 tar -xf $selfChosenFile
+    transferFile
   else 
     echo "The file does not exists"
     exit 1
   fi ;;
 *)
-  echo "Sorry we did not find you number please enter a valid number between 1 and 6" ;;
+  echo "Sorry we did not find you number please enter a valid number between 1 and 2" ;;
 
 esac
