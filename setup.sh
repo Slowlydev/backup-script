11480d4d85fece647bed0aa8f0951166ae200f2d
Slowlydev <slowly.developers@gmail.com>
2022-05-05 18:38:34 +0200

    feat: added cronjob option

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ storeToConfig() {
   sed -i '' "s/\(${1} *= *\).*/\1${2//\//\\/}/" backup.config
 }
 
+storeToCrontab() {
+  crontab -l | { cat echo "${1}" } | crontab -
+}
+
 # check if backup file exsists
 if [[ -s "backup.config" ]]; then
   source "backup.config"
@@ -19,9 +23,7 @@ else
 fi
 
 # welcome message
-echo ""
-echo "Welcome to the backup setup"
-echo ""
+echo "\n Welcome to the backup setup \n"
 
 # get and save directory for backups 
 echo "Please enter the directory where u want to store your compressed backups"
@@ -33,3 +35,15 @@ echo "Please enter the directory u want to backup"
 read -p "Directory to backup: " targetDirectory
 storeToConfig "targetDirectory" $targetDirectory
 
+# Ask for cronjob setup
+read -p "Do u want to run your backups automatically daily with a cronjob? " cronjobAnswer
+
+# Check for answer
+if [[ $cronjobAnswer =~ ^[Yy]$ ]]
+then
+  echo "info: set scripts direcory"
+  # store direcory of scripts
+  scriptsDirectory=$(pwd)
+  echo "info: write daily backup to crontab"
+  StoreCrontab "0 0 * * * cd ${scriptsDirectory} && ./backup.sh"
+fi
